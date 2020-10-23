078f86a89e6ae6efaef0f5b5f1afda5d7561a0b3
Sarina Lyons <sarinalyons@gmail.com>
2020-10-23 15:11:14 -0400

    updates

--- Python-Game/CardClass.py
+++ Python-Game/CardClass.py
@@ -4,20 +4,11 @@ import random
 ##set up of all basic classes
 class Card:
     def __init__(self, suits, values, suitnums):
-        self.suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "Q", "K", "J"]
-        self.suitnums = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
+        self.suits = suits
+        self.values = values
+        self.suitnums = suitnums
 
-        ##set deck placeholder
-        deck = []
-
-
-        ##deck set up
-        for suit in suits:
-            for value in values:
-                deck.append(Card(suit.values[suits], values, suitnums[values]))
         
-                 
 class PlayerHand:
     def __init__(self, cards):
         self.cards = cards
@@ -25,6 +16,23 @@ class PlayerHand:
 class Dealer:
     pass
 
+
+
+##lists and dictionaries of card and card values
+suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "Q", "K", "J"]
+suitnums = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
+
+##set deck placeholder
+        deck = []
+
+
+##deck set up
+for suit in suits:
+    for value in values:
+        deck.append(Card(suit.values[suits], value, suitnums[values]))
+        
+                 
 
 
         
