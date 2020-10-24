dcb68816a9d85571b4a30e3b01cf8e0d3de63fbc
Sarina Lyons <sarinalyons@gmail.com>
2020-10-23 21:59:16 -0400

    MenuStartDraft

---
+++ Python-Game/MenuDraft.py
@@ -0,0 +1,16 @@
+while True:
+
+    print("WELCOME TO THE BLACKJACK GAME!"
+    choice = int(input("""
+        1. Start Game
+        2. View Instructions
+        3. Exit
+        """))
+    if choice == 1:
+        play_blackjack(deck)
+    elif choice == 2:
+        view_instructions()
+    else:
+        pass
+
+  
